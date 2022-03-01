f2f60db198f4236f549313926f4687dddc673466
Camilo Castro <camilo.castrotech@gmail.com>
2022-02-28 19:06:04 -0500

    Initial commit

---
+++ src/main/java/com/concesionario/autosbackend/BackendConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package com.concesionario.autosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConcesionarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendConcesionarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/concesionario/autosbackend/BackendConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concesionario.autosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendConcesionarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
